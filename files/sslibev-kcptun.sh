f651085a0fa2b566b4657dabb698aa169342b641
Lyraa <xzl2021@hotmail.com>
2020-04-09 17:31:16 +0800

    Update sslibev-kcptun.sh

--- files/sslibev-kcptun.sh
+++ files/sslibev-kcptun.sh
@@ -7,8 +7,8 @@ if [ ! -f /etc/shadowsocks-libev/kcptun.json ]; then
 	cp /kcptun.json /etc/shadowsocks-libev/kcptun.json
 fi
 
-ss-server -v -c /etc/shadowsocks-libev/config.json -f /var/run/shadowsocks-libev.pid
-kcptun -c /etc/shadowsocks-libev/kcptun.json
+ss-server -v -c /etc/shadowsocks-libev/config.json -f /var/run/shadowsocks-libev.pid || exit 1
+kcptun -c /etc/shadowsocks-libev/kcptun.json || exit 1
 
 while true
 do
