0441b51ff62f8cf03848bca9999c4925568f9d92
Lyraa <xzl2021@hotmail.com>
2022-10-06 11:19:03 +0800

    Update to use 0.0.0.0 to only bind ipv4 port.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /etc/shadowsocks-libev-kcptun/
-docker run -d -p 9443:443/udp -p 9444:444/udp -v /etc/shadowsocks-libev-kcptun:/etc/shadowsocks-libev --restart=always --name ss-kcp sslibev-kcptun:latest
+docker run -d -p 0.0.0.0:9443:443/udp -p 0.0.0.0:9444:444/udp -v /etc/shadowsocks-libev-kcptun:/etc/shadowsocks-libev --restart=always --name ss-kcp sslibev-kcptun:latest
